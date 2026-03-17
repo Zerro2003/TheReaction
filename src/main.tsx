b1d9bdaf49bd961b18ce11d44f02d57982bb5915
Zerro2003 <mugishaelie160@gmail.com>
2026-03-17 18:00:00 +0200

    feat: wire up data mapping in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import NewFacts from "./nav.tsx";
+import Sim from "./sim.tsx";
+import { myData } from "./data.tsx";
+import Chef from "./chef.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <NewFacts/>
+    {myData.map((item)=>{
+      return(
+        <Sim
+        key={item.id}
+        name={item.name}
+        age={item.age}
+        email={item.email}
+        phone={item.phone}
+        />
+      )
+    })}
+    <Chef/>
   </StrictMode>,
 );
