241cbb098404a3f250ea9a460fd13fa83909b3e2
Zerro2003 <mugishaelie160@gmail.com>
2026-03-19 15:30:00 +0200

    refactor: update main.tsx imports for conditional rendering components

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import NewFacts from "./nav.tsx";
-import Sim from "./sim.tsx";
-import { myData } from "./data.tsx";
-import Chef from "./chef.tsx";
-import Space from "./space.tsx";
-import TestState from "./testState.tsx";
+import SmartLamp from "./myLamp.tsx";
+import Write from "./writeCurrent.tsx";
+import TitleCounter from "./titleCounter.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <NewFacts/>
-    <Chef/>
-    <Space/>
-    <TestState/>
+    <SmartLamp />
+    <Write />
+    <TitleCounter />
   </StrictMode>,
 );
