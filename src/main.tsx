2e31710fa1e0390044798633da442cb74e46e788
Zerro2003 <mugishaelie160@gmail.com>
2026-03-24 17:00:00 +0200

    refactor: final main.tsx with all component demos

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Accordion from "./accordion.tsx";
-import CardWrapper from "./cardWrapper.tsx";
-import ProgressBar from "./progressBar.tsx";
+import Write from "./writeCurrent.tsx";
+import TitleCounter from "./titleCounter.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CardWrapper title="Component Composition Demo">
-      <Accordion />
-    </CardWrapper>
-    <ProgressBar value={75} label="React Progress" />
+    <Write />
+    <TitleCounter/>
   </StrictMode>,
 );
